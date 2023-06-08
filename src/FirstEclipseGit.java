5158ac94f3cc191f37a2dc566273732947411d2e
revanthpotluri <potlurirevanthchowdary@gmail.com>
2023-06-07 21:16:35 -0400

    Second Commit

--- src/FirstEclipseGit.java
+++ src/FirstEclipseGit.java
@@ -3,6 +3,7 @@ public class FirstEclipseGit {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to First Eclipse Project using Git");
+		System.out.println("Welcome to First Eclipse Project using Git");
 
 	}
 
